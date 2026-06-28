3704ef9cdecbf275f0a6a898e2e4f947b68c9a7e
AboElsoad-SWE <montage.abosoad@gmail.com>
2026-06-28 19:46:17 +0300

    fix install

--- src/View/src/Test.jsx
+++ src/View/src/Test.jsx
@@ -8,7 +8,7 @@ export const Form = () => {
 
     const schema = z.object({
         name: z.string().min(2, "Name must be at least 2 characters long"),
-        email: z.email("Invalid email address"),
+        email: z.string().email("Invalid email address"),
         password: z.string().min(6, "Password must be at least 6 characters long"),
     })
 
