dfe22dce5c3d324437de24d24252e613cd7122c4
Ahmed Salem <salembayern889@gmail.com>
2026-07-06 00:44:32 +0300

    last touches

--- src/View/src/components/Skills.jsx
+++ src/View/src/components/Skills.jsx
@@ -1,31 +1,48 @@
+import {useState} from "react" 
 export function Skills({selectedSkills, setSelectedSkills}) {
-  const skills = [
+  const [customSkill, setCustomSkill] = useState('');
+  const defaultSkills = [
     "React", "TypeScript", "Node.js", "Python", "AWS",
     "Docker", "PostgreSQL", "GraphQL", "Next.js",
     "Tailwind", "MongoDB", "Redis"
   ];
 
-  
+
   let toggleSkill = (skill) => {
     if (!selectedSkills.includes(skill)) {
-      setSelectedSkills([...selectedSkills, skill])
+      setSelectedSkills((prev)=>([...prev, skill]))
     }
+    else{
+      setSelectedSkills((prev) => prev.filter(s => s !== skill))
+    }
+  }
+  function handleAddCustomSkill (e) {
+    e.preventDefault();
+    setSelectedSkills(prev=>([...prev, customSkill]))
+    setCustomSkill('');
   }
   return (
     <div className="form-box">
       <h3 className="section-title">Select Your Skills</h3>
       <div className="skills-grid">
-        {skills.map((skill, index) => (
+        {defaultSkills.map((skill, index) => (
           <button key={index} onClick={() => {toggleSkill(skill)}} type="button" className={`skill-pill ${selectedSkills.includes(skill) ? 'active' : ''}`}>
             {skill}
           </button>
         ))}
       </div>
-      <form action="" onSubmit={(e) => e.preventDefault()}>
+
+      <form onSubmit={handleAddCustomSkill}>
         <div className="form-group">
           <label htmlFor="custom-skill">Add Custom Skill</label>
           <div className="add-skill-row">
-            <input id='custom-skill' type="text" placeholder='e.g., Kubernetes, GraphQL...' />
+            <input
+              id='custom-skill'
+              type="text"
+              placeholder='e.g., Kubernetes, GraphQL...'
+              value={customSkill}
+              onChange={(e) => setCustomSkill(e.target.value)}
+            />
             <button type='submit' className="btn-add">Add</button>
           </div>
         </div>
